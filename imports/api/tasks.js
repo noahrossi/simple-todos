4671a854a7fff1ac79cde952c359644abf4ecc53
Noah Rossi <me@noahrossi.com>
2016-04-08 21:57:32 -0400

    security

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,33 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
  
 export const Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isServer) {
+    Meteor.publish('tasks', function tasksPublication() {
+       return Tasks.find();
+    });
+}
+
+Meteor.methods({
+    'tasks.insert'(text) {
+        check(text, String);
+        
+        Tasks.insert({
+            text,
+            createdAt: new Date()
+        });
+    },
+    'tasks.remove'(taskId) {
+        check(taskId, String);
+        
+        Tasks.remove(taskId);
+    },
+    'tasks.setChecked'(taskId, setChecked) {
+        check(taskid, String);
+        check(setChecked, Boolean);
+        
+        Tasks.update(taskId, { $set: { checked: setChecked } });
+    },
+});
